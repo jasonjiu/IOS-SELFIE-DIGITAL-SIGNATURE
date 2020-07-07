a927b61b59cb5a47e05b8e5224c8c9022239fe94
jason anggarah <jasonanggarah@gmail.com>
2020-07-07 11:05:15 +0700

    Initial Commit

---
+++ Asliri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Asliri
+//
+//  Created by PROSIA on 07/07/20.
+//  Copyright © 2020 PROSIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
